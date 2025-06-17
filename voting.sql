d477b96719f9a995d1a7e1cdbc5c58ba899668fa
AJericSixteen <154009866+AJericSixteen@users.noreply.github.com>
2025-06-17 21:12:14 +0800

    Add files via upload

---
+++ voting.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 17, 2025 at 03:06 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `voting`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidate`
+--
+
+CREATE TABLE `candidate` (
+  `candidate_id` int(11) NOT NULL,
+  `position` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `year_level` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `party` varchar(50) NOT NULL,
+  `img` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `candidate`
+--
+
+INSERT INTO `candidate` (`candidate_id`, `position`, `firstname`, `lastname`, `year_level`, `gender`, `party`, `img`) VALUES
+(39, 'President', 'Candidate1', 'Candidate1', '1st Year', 'Male', 'Test', 'upload/laravel-icon-1990x2048-xawylrh0.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `firstname`, `lastname`) VALUES
+(1, 'admins', '$2y$10$KUanIbPDTIQj.SQ97QfSPu4BNQSjzdGY6xxexxx.HH13j3pXbAGly', 'admin', 'admin'),
+(8, 'test', '$2y$10$G5F7vca8Nj67bvu1PGuFd..7hCjDmgSjMWtDcdSL2QKR2ZI8G1vly', 'test', 'test');
+
+--
+-- Triggers `user`
+--
+DELIMITER $$
+CREATE TRIGGER `prevent_delete_user1` BEFORE DELETE ON `user` FOR EACH ROW BEGIN
+    IF OLD.user_id = 1 THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Cannot delete the protected user (user_id = 1).';
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voters`
+--
+
+CREATE TABLE `voters` (
+  `voters_id` int(11) NOT NULL,
+  `id_number` int(11) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `year_level` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `account` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `voters`
+--
+
+INSERT INTO `voters` (`voters_id`, `id_number`, `password`, `firstname`, `lastname`, `year_level`, `status`, `account`) VALUES
+(1246, 1123, 'D5LozWr5', 'test', 'test', 'Grade 11', 'Voted', 'Active'),
+(1247, 12345, 'l3XrEXF2', 'Gilbert', 'Cabasag', '1st Year', 'Voted', 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `vote_id` int(255) NOT NULL,
+  `candidate_id` varchar(255) NOT NULL,
+  `voters_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`vote_id`, `candidate_id`, `voters_id`) VALUES
+(1, '1', '2'),
+(2, '', '2'),
+(3, '', '2'),
+(4, '2', '2'),
+(5, '', '2'),
+(6, '', '2'),
+(7, '', '2'),
+(8, '', '2'),
+(9, '', '2'),
+(10, '', '2'),
+(11, '', '2'),
+(12, '1', '1235'),
+(13, '', '1235'),
+(14, '2', '1235'),
+(15, '', '1235'),
+(16, '', '1235'),
+(17, '', '1235'),
+(18, '', '1235'),
+(19, '', '1235'),
+(20, '', '1235'),
+(21, '', '1235'),
+(22, '', '1235'),
+(23, '39', '1247'),
+(24, '', '1247'),
+(25, '', '1247'),
+(26, '', '1247'),
+(27, '', '1247'),
+(28, '', '1247'),
+(29, '', '1247'),
+(30, '', '1247'),
+(31, '', '1247'),
+(32, '', '1247'),
+(33, '', '1247'),
+(34, '39', '1246'),
+(35, '', '1246'),
+(36, '', '1246'),
+(37, '', '1246'),
+(38, '', '1246'),
+(39, '', '1246'),
+(40, '', '1246'),
+(41, '', '1246'),
+(42, '', '1246'),
+(43, '', '1246'),
+(44, '', '1246');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `candidate`
+--
+ALTER TABLE `candidate`
+  ADD PRIMARY KEY (`candidate_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `voters`
+--
+ALTER TABLE `voters`
+  ADD PRIMARY KEY (`voters_id`);
+
+--
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`vote_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `candidate`
+--
+ALTER TABLE `candidate`
+  MODIFY `candidate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `voters`
+--
+ALTER TABLE `voters`
+  MODIFY `voters_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1248;
+
+--
+-- AUTO_INCREMENT for table `votes`
+--
+ALTER TABLE `votes`
+  MODIFY `vote_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
